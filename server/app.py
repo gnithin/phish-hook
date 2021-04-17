02e06a2e7ff75486584374529af56d1cae137682
Nithin <nithin.linkin@gmail.com>
2021-04-17 14:42:18 -0400

    [server] Check for root

--- server/app.py
+++ server/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from urllib.parse import urlparse
+
 app = Flask(__name__)
 
 @app.route('/detect', methods=['POST'])
@@ -7,7 +9,8 @@ def detect():
     resp = {
             "success": True,
             "message": "",
-            "is_malicious": False
+            "is_malicious": False,
+            "payload": "",
             }
     if not url:
         resp["success"] = False
@@ -15,12 +18,15 @@ def detect():
     else:
         resp["success"] = True
         resp["is_malicious"] = is_malicious(url)
+        resp["payload"] = url
     
     return jsonify(resp)
 
 def is_malicious(url):
     # TODO: Add ML predict logic here
-    return url == "http://stackoverflow.com"
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    return domain == "stackoverflow.com"
 
 
 if __name__ == '__main__':
