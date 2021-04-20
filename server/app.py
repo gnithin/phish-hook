4cb242b0ee55de416b30359e756f8f559df34e6b
Nithin <nithin.linkin@gmail.com>
2021-04-20 13:17:41 -0400

    [decision-tree] Use the ml classifier in the code

--- server/app.py
+++ server/app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 from urllib.parse import urlparse
 from features import get_features_for_url
 
+import pickle
+import numpy as np
+
 app = Flask(__name__)
+clf = None
 
 @app.route('/detect', methods=['POST'])
 def detect():
@@ -24,10 +28,28 @@ def detect():
     return jsonify(resp)
 
 def is_malicious(url):
-    # TODO: Add ML predict logic here
+    # Add ML predict logic here
+    global clf
+    print(f"Processing ${url}")
+    print(clf)
     features = get_features_for_url(url)
-    res = clf.predict(features)[0]
-    return res == 1
+    print(features)
+    feature_input = np.array(features).reshape(1,-1)
+    res = clf.predict(feature_input)
+    print(res)
+    if res[0] == 1:
+        return True
+    return False
+
+
+def setup_model():
+    global clf
+    with open('./model.pkl', 'rb') as f:    # load
+        clf = pickle.load(f)
 
 if __name__ == '__main__':
+    # build the model
+    setup_model()
+    print("Loaded the model!")
+    print("Running the server")
     app.run(host='0.0.0.0', port=9999)
