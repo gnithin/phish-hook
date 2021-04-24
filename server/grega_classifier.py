165b778d6c1d969b3241dde229c32b119bae76bd
Nithin <nithin.linkin@gmail.com>
2021-04-24 13:44:59 -0400

    [combine] Get the size model working

--- server/grega_classifier.py
+++ server/grega_classifier.py
@@ -1,17 +1,18 @@
 import pickle
 from features import get_features_for_url
 from abstract_classifier import AbstractClassifier
+import numpy as np
 
 
 class GregaClassifier(AbstractClassifier):
-    def __new__(self, modelPath):
+    def __init__(self, modelPath):
         with open("./model.pkl", "rb") as f:
-            grega_clf = pickle.load(f)
+            self.clf = pickle.load(f)
 
     def predict(self, url) -> bool:
         features = get_features_for_url(url)
         feature_input = np.array(features).reshape(1, -1)
-        res = clf.predict(feature_input)
+        res = self.clf.predict(feature_input)
         if res[0] == 1:
             return True
         return False
