d0349ddfc242cea33244bb12f1cc632670704e51
Nithin <nithin.linkin@gmail.com>
2021-04-25 13:24:06 -0400

    [combined] Add a react component

--- extension/src/browser_action/warning.js
+++ extension/src/browser_action/warning.js
@@ -1,18 +1,46 @@
 window.onload = () => {
     console.log("Page loaded!")
 
-    // get id from url 
-    const urlParams = new URLSearchParams(window.location.search);
-    const id = urlParams.get('id');
-    if (id !== null || typeof (id) !== undefined) {
-        loadData(id)
+    ReactDOM.render(
+        React.createElement(Warning, {}),
+        document.getElementById('root')
+    );
+};
+
+class Warning extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null,
+        };
     }
-}
 
-const loadData = (id) => {
-    console.log("Loading data for id - ", id);
+    componentDidMount() {
+        // get id from url 
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get('id');
+        if (id !== null || typeof (id) !== undefined) {
+            this.loadData(id)
+        }
+    }
 
-    chrome.storage.local.get([id], function (result) {
-        console.log('Value currently is ', result);
-    });
+    loadData(id) {
+        console.log("Loading data for id - ", id);
+        chrome.storage.local.get([id], (result) => {
+            console.log('Value currently is ', result);
+            // Change the state
+            this.setState({ data: result[id] });
+        });
+    }
+
+    render() {
+        let url = ""
+        if (this.state.data) {
+            url = this.state.data.url;
+        }
+        return React.createElement(
+            'h1', null, 'Greetings, ' + url
+        );
+    }
 }
